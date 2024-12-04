700cb204d8d0b9536e43d56ff3e4062f00c51d79
Brandon33as <marquezaguilarbrandonalexander@gmail.com>
2024-12-04 02:17:48 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".mySwiper-1", {
+    slidesPerView:1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el:"Swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev",
+    }
+})
+
+  
